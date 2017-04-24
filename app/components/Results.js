885929c28725ab0218490272dc8922905f2767bf
hienvd <duchienvuong@gmail.com>
2017-04-25 00:15:23 +0700

    props.childrend

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,6 +1,47 @@
 var React = require('react');
 var queryString = require('query-string');
+var PropTypes = require('prop-types');
 var api = require('../utils/api');
+var Link = require('react-router-dom').Link;
+var PlayerPreview = require('./PlayerPreview');
+
+function Profile (props) {
+  var info = props.info;
+
+  return (
+    <PlayerPreview username={info.login} avatar={info.avatar_url}>
+      <ul className='space-list-items'>
+        {info.name && <li>{info.name}</li>}
+        {info.location && <li>{info.location}</li>}
+        {info.company && <li>{info.company}</li>}
+        <li>Followers: {info.followers}</li>
+        <li>Following: {info.following}</li>
+        <li>Public Repos: {info.public_repos}</li>
+        {info.blog && <li><a href={info.blog}>{info.blog}</a></li>}
+      </ul>
+    </PlayerPreview>
+  )
+}
+
+Profile.propTypes = {
+  info: PropTypes.object.isRequired,
+}
+
+function Player (props) {
+  return(
+    <div>
+      <h1 className='header'> {props.label} </h1>
+      <h3 style={{textAlign: 'center'}}> {props.score} </h3>
+      <Profile info={props.profile}/>
+    </div>
+  )
+}
+
+Player.propTypes =  {
+  label: PropTypes.string.isRequired,
+  score: PropTypes.number.isRequired
+}
+
 
 class Results extends React.Component {
   constructor (props) {
@@ -20,9 +61,25 @@ class Results extends React.Component {
     api.battle([
       players.playerOneName,
       players.playerTwoName
-    ]).then(function(aa) {
-      console.log(aa);
-    })
+    ]).then(function(results) {
+      if(results === null) {
+        return this.setState(function(){
+          return {
+            error: 'Look like there was an error. Check both username exists in Github',
+            loading: false
+          }
+        });
+      }
+
+      this.setState(function() {
+        return {
+          error: null,
+          winner: results[0],
+          loser: results[1],
+          loading: false
+        }
+      })
+    }.bind(this));
   }
 
   render() {
@@ -35,8 +92,28 @@ class Results extends React.Component {
       return <p> Loading </p>
     }
 
+    if (error) {
+      return (
+        <div>
+          <p> {error} </p>
+          <Link to='/battle'> Reset </Link>
+        </div>
+      )
+    }
+
     return (
-      <p>Show results here!</p>
+      <div className='row'>
+        <Player
+          label='Winner'
+          score={winner.score}
+          profile={winner.profile}
+        />
+        <Player
+          label='Loser'
+          score={loser.score}
+          profile={loser.profile}
+        />
+      </div>
     )
   }
 }
