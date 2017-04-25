2b43f2ade917fdf0ca2485d91db974f4d0518e10
hienvd <duchienvuong@gmail.com>
2017-04-25 21:29:58 +0700

    Finish loading...

--- app/components/Loading.js
+++ app/components/Loading.js
@@ -16,6 +16,30 @@ class Loading extends React.Component {
       text: props.text
     }
   }
+
+  componentDidMount() {
+    let stopper = this.props.text + '...';
+    this.interval = window.setInterval(function() {
+      if(this.state.text === stopper) {
+        this.setState(function() {
+          return {
+            text: this.props.text
+          }
+        })
+      } else {
+        this.setState(function(prevState) {
+          return {
+            text: prevState.text +  '.'
+          }
+        })
+      }
+    }.bind(this), 300);
+  }
+
+  componentWillUnmount() {
+    window.clearInterval(this.interval);
+  }
+
   render() {
     return (
       <p style={styles.content}>
